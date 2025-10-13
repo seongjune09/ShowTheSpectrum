ed49dbae4e0551945007ea3982cf428bfb238277
박성준 <freestarjun@gmail.com>
2025-10-13 18:12:55 +0900

    first

---
+++ Spectrum/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../styles/main.css";
+export default function Home() {
+    return (
+      <main>
+
+        <div className = "Main-div">
+          <img src = "Main-Logo.svg" className = "Main-Logo"></img>
+        </div>
+        
+      </main>
+    );
+  }
